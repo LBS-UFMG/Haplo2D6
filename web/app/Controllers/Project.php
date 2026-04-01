e32e208f499093fe5d9c714cb9075b914668e630
Diego Mariano <diegomariano@ufmg.br>
2026-03-31 22:27:27 -0300

    Update Project.php

--- web/app/Controllers/Project.php
+++ web/app/Controllers/Project.php
@@ -83,7 +83,7 @@ class Project extends BaseController
                     .'*5'.',' # allele2
                     .'0'.',' # activity2
                     .$activity1.',' # score 
-                    .Project::calculate_phenotype($activity1).'<br>';
+                    .Project::calculate_phenotype($activity1)."\n";
                 fwrite($w, $texto);
             }
             // condição: se haplotype1 == haplotype2 and $cnv[$id] > 2
@@ -100,7 +100,7 @@ class Project extends BaseController
                     .$allele2.',' # allele2
                     .$activity2.',' # activity2
                     .$activity1*$cnv[$id].',' # score 
-                    .Project::calculate_phenotype($activity1*$cnv[$id]).'<br>';
+                    .Project::calculate_phenotype($activity1*$cnv[$id])."\n";
                 fwrite($w, $texto);
             }
             else{
